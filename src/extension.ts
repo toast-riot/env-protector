7c98d0bf6dfdb0b04489e49e3e7faabf40d92dc1
Anonymous <c83081f5@bVtzKmXyOp.com>
2025-08-07 18:57:01 +1000

    feat: syntax highlighting, css

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode'
 import { getMaskedView } from './utils/get-masked-view'
 import { name } from '.././package.json'
-import fs from 'fs'
 import { addEnvVariable } from './commands/add-env-variable'
 import { maskEnvValues } from './commands/mask-env-values'
 import { showEnvFiles } from './commands/show-env-files'
@@ -9,79 +8,23 @@ import { hideEnvFiles } from './commands/hide-env-files'
 import { editEnvVariable } from './commands/edit-env-variable'
 import { removeEnvVariable } from './commands/remove-env-variable'
 
-/**
- * This method is called when your extension is activated
- * Your extension is activated the very first time the command is executed
- */
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(`${name} is now active!`)
-
-  // Register the custom editor provider for the .env files
+  console.log('Activated')
+  
   vscode.window.registerCustomEditorProvider(`${name}.envFileEditor`, {
     async resolveCustomTextEditor(document, webviewPanel) {
-      /**
-       * Selection options for the user to choose from
-       */
-      const options: vscode.QuickPickItem[] = [
-        {
-          label: 'Open with masked',
-          description: 'Mask environment values before opening the file',
-        },
-        {
-          label: 'Open Anyway',
-          description: 'Open file with built in editor',
-        },
-      ]
 
-      const quickPick = vscode.window.createQuickPick()
-      quickPick.items = options.map((option) => ({
-        label: option.label,
-        description: option.description,
-      }))
-      quickPick.title = 'Before opening .env file'
-      quickPick.placeholder = 'Choose an action to perform:'
-      quickPick.ignoreFocusOut = true
-      quickPick.canSelectMany = false
-
-      const disposables: vscode.Disposable[] = []
-
-      // Close quick pick if the opened another file
-      disposables.push(
-        vscode.window.onDidChangeActiveTextEditor(() => {
-          quickPick.hide()
-        }),
-      )
-
-      quickPick.onDidAccept(async () => {
-        const selectedItem = quickPick.selectedItems[0]
-        if (selectedItem?.label === 'Open Anyway') {
-          // Reopen the document in the default text editor without asking
-          await vscode.commands.executeCommand(
-            'vscode.openWith',
-            document.uri,
-            'default',
-          )
-          webviewPanel.dispose()
-        } else {
-          const text = document.getText()
-          webviewPanel.webview.html = getMaskedView(text, name)
-        }
-        quickPick.hide()
-        disposables.forEach((disposable) => disposable.dispose())
-      })
-
-      quickPick.onDidHide(() => {
-        disposables.forEach((disposable) => disposable.dispose())
-        quickPick.dispose()
-      })
-
-      quickPick.show()
-    },
+      const text = document.getText()
+      webviewPanel.webview.html = getMaskedView(text, context, webviewPanel)
+      webviewPanel.webview.options = {
+        localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'src', 'css')],
+      }
+    }
+  },
+  {
+    webviewOptions: { retainContextWhenHidden: true }
   })
-
-  // This line of code will only be executed once when your extension is activated
+  
   context.subscriptions.push(
     showEnvFiles,
     maskEnvValues,
@@ -92,9 +35,6 @@ export function activate(context: vscode.ExtensionContext) {
   )
 }
 
-/**
- * This method is called when your extension is deactivated
- */
 export function deactivate() {
   console.log('Deactivated')
 }
--- src/utils/get-masked-view.ts
+++ src/utils/get-masked-view.ts
@@ -1,92 +1,132 @@
 import { JSDOM } from 'jsdom'
+import * as vscode from 'vscode'
+
+function getMaskedValue(value: string): string {
+  return '*******'
+}
+
+function parseEnv(content: string) {
+  const match = content.match(
+    /\s*(?:([\w.-]+)\s*(=)\s*(?:(['"])((?:\\.|(?!\3).)*?)\3|([^\#\n\r]*)))?\s*(?:#(.*))?/
+  );
+
+  if (!match) return null
+  const key = match[1]
+  const equalSign = match[2]
+  const quoteType = match[3]
+  const value = match[4] ?? match[5]
+  const comment = match[6]
+  return { key, value, comment, equalSign, quoteType }
+}
 
 /**
  * Generates an HTML view with masked values from a given text and a name.
  *
  * @param text - The input text containing key-value pairs, typically from a .env file.
- * @param name - The name to be displayed in the generated HTML.
+ * @param context - The extension context providing extensionUri.
+ * @param webviewPanel - The webview panel to resolve resource URIs.
  * @returns The generated HTML string with masked values.
  *
  * The function creates an HTML document with a paragraph indicating the values are masked,
  * followed by a preformatted block containing the masked key-value pairs. If the input text
  * is empty, it indicates that no content was found in the .env file.
  */
-export function getMaskedView(text: string, name: string): string {
+export function getMaskedView(
+  text: string,
+  context: vscode.ExtensionContext,
+  webviewPanel: vscode.WebviewPanel
+): string {
   const dom = new JSDOM()
   const htmlDocument = dom.window.document
-  const p = htmlDocument.createElement('p')
 
-  const a = htmlDocument.createElement('a')
-  a.textContent = name
-  a.href = 'https://sametcc.me/env-protector'
+  const cssPath = vscode.Uri.joinPath(context.extensionUri, 'src' ,'css', 'masked_view.css');
+  const cssUri = webviewPanel.webview.asWebviewUri(cssPath);
 
-  p.appendChild(htmlDocument.createTextNode('Values masked by '))
-  p.appendChild(a)
-  p.appendChild(
-    htmlDocument.createTextNode(
-      '. Here are the masked values in the .env file',
-    ),
-  )
+  const stylesheetLink = htmlDocument.createElement('link')
+  stylesheetLink.rel = 'stylesheet'
+  stylesheetLink.type = 'text/css'
+  stylesheetLink.href = cssUri.toString()
+  htmlDocument.head.appendChild(stylesheetLink)
 
   const pre = htmlDocument.createElement('pre')
 
-  if (text.length === 0) {
-    const code = htmlDocument.createElement('code')
-    code.textContent = 'No content found in the .env file'
-    pre.appendChild(code)
-    htmlDocument.body.appendChild(p)
-    htmlDocument.body.appendChild(pre)
-    return htmlDocument.documentElement.outerHTML
-  }
-
-  let keyCount: number = 0
+  // if (text.length === 0) {
+  //   const code = htmlDocument.createElement('code')
+  //   code.textContent = 'No content found in the .env file'
+  //   pre.appendChild(code)
+  //   htmlDocument.body.appendChild(pre)
+  //   return htmlDocument.documentElement.outerHTML
+  // }
 
   const lines = text.split('\n')
   for (const line of lines) {
-    const trimmedLine = line.trim()
-    const singleLineComment = htmlDocument.createElement('span')
-    if (trimmedLine.startsWith('#') || trimmedLine.length === 0) {
-      singleLineComment.textContent = line
-      singleLineComment.style.color = 'gray'
-      pre.appendChild(singleLineComment)
-    } else {
-      const [keyValuePart, commentPart] = line.split('#')
-      const [key, value] = keyValuePart.split('=')
 
-      const keySpan = htmlDocument.createElement('span')
-      keySpan.style.fontWeight = 'bold'
-      keySpan.textContent = key
-
-      const equalSignSpan = htmlDocument.createElement('span')
-      equalSignSpan.textContent = '='
-
-      const valueSpan = htmlDocument.createElement('span')
-      valueSpan.textContent = value.replace(/./g, '*')
-      valueSpan.textContent = ' ' + valueSpan.textContent
-
-      const lineDiv = htmlDocument.createElement('div')
-      lineDiv.appendChild(keySpan)
-      lineDiv.appendChild(equalSignSpan)
-      lineDiv.appendChild(valueSpan)
-
-      if (commentPart) {
-        const commentSpan = htmlDocument.createElement('span')
-        commentSpan.textContent = ` #${commentPart}`
-        commentSpan.style.color = 'gray'
-        lineDiv.appendChild(commentSpan)
-      }
-
-      pre.appendChild(lineDiv)
-      keyCount++
+    if (line.trim() === '') {
+      console.warn('Skipping blank line:', line)
+      continue
     }
+
+    const parsed = parseEnv(line)
+    if (!parsed) continue
+    const { key, value, comment, equalSign, quoteType } = parsed
+
+    // if (Object.values(parsed).every(v => v === undefined)) {
+    //   console.warn('blank line', line)
+    //   continue
+    // }
+
+    // console.log('Parsed line:', parsed)
+
+    // line
+    const lineDiv = htmlDocument.createElement('div')
+    lineDiv.className = 'line'
+
+    // key
+    if (key !== undefined) {
+      const keySpan = htmlDocument.createElement('span')
+      keySpan.textContent = key
+      lineDiv.appendChild(keySpan)
+    }
+
+    // equals
+    if (equalSign !== undefined) {
+      const equalSignSpan = htmlDocument.createElement('span')
+      equalSignSpan.className = 'token-operator'
+      equalSignSpan.textContent = equalSign
+      lineDiv.appendChild(equalSignSpan)
+    }
+
+    // quotes
+    if (quoteType !== undefined) {
+      const quoteSpan = htmlDocument.createElement('span')
+      quoteSpan.className = 'token-operator'
+      quoteSpan.textContent = quoteType
+      lineDiv.appendChild(quoteSpan)
+    }
+
+    // value
+    if (value !== undefined) {
+      const valueSpan = htmlDocument.createElement('span')
+      valueSpan.textContent = getMaskedValue(value)
+      lineDiv.appendChild(valueSpan)
+    }
+
+    if (quoteType !== undefined) { // TODO: prevent code re-use
+      const quoteSpan2 = htmlDocument.createElement('span')
+      quoteSpan2.className = 'token-operator'
+      quoteSpan2.textContent = quoteType
+      lineDiv.appendChild(quoteSpan2)
+    }
+
+    if (comment !== undefined) {
+      const commentSpan = htmlDocument.createElement('span')
+      commentSpan.className = 'token-comment'
+      commentSpan.textContent = `${value ? ' ' : ''}#${comment}`
+      lineDiv.appendChild(commentSpan)
+    }
+
+    pre.appendChild(lineDiv)
   }
-  const br = htmlDocument.createElement('br')
-  p.appendChild(br)
-  const span = htmlDocument.createElement('i')
-  span.style.fontStyle = 'italic'
-  span.textContent = `${keyCount} masked key-value pairs in the .env file`
-  p.appendChild(span)
-  htmlDocument.body.appendChild(p)
   htmlDocument.body.appendChild(pre)
   return htmlDocument.documentElement.outerHTML
 }
